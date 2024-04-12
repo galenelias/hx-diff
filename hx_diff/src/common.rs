5341cdb6e610c23468f65dc13d0c20f5e67a758e
Galen Elias <gelias@gmail.com>
2024-04-11 21:28:44 -0700

    cargo update

--- hx_diff/src/common.rs
+++ hx_diff/src/common.rs
@@ -1,16 +1,16 @@
 use gpui::*;
 
-pub static WIDTH: f64 = 1024.0;
-pub static HEIGHT: f64 = 600.0;
+pub static WIDTH: i32 = 1024;
+pub static HEIGHT: i32 = 600;
 
 // Setup window helper from Duane Bester https://github.com/duanebester which is based on work
 // from Matthias from  // https://github.com/MatthiasGrandl/Loungy
-pub fn setup_window(app_width: f64, app_height: f64, cx: &mut AppContext) -> WindowOptions {
+pub fn setup_window(app_width: i32, app_height: i32, cx: &mut AppContext) -> WindowOptions {
 	let mut options = WindowOptions::default();
 
-	let width = GlobalPixels::from(app_width);
-	let height = GlobalPixels::from(app_height);
-	options.bounds = Some(Bounds::centered(Size { width, height }, cx));
+	let width = DevicePixels::from(app_width);
+	let height = DevicePixels::from(app_height);
+	options.bounds = Some(Bounds::centered(None, Size { width, height }, cx));
 	options.titlebar = Some(TitlebarOptions {
 		title: Some("HxDiff".into()),
 		..Default::default()
